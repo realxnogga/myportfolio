2a1b0c0bf65d4040959c5bef5158ad8362e5d2d9
realxnogga <larioswarren5@gmail.com>
2024-03-15 23:33:10 +0800

    lmao

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,8 +5,16 @@ import { Avatar, Dropdown, Navbar } from 'flowbite-react';
 
 export const Header = () => {
 
+  const navLinkDesign = 'mobile:text-[2rem] mobile:py-5 mobile:pl-0'
+
   return (
-    <Navbar fluid className='bg-gray-900 w-screen backdrop-blur backdrop-brightness-75 px-10'>
+    <Navbar fluid className='
+    mobile:bg-black
+    mobile:bg-opacity-90
+    mobile:
+    mobile:
+
+    bg-gray-900 w-screen backdrop-blur backdrop-brightness-75 px-10'>
       <Navbar.Brand>
         <img className="animate-spin spin h-[3rem] w-[3rem] mobile:h-[2rem] mobile:w-[2rem]" src={`${require('../assets/icons/logo192.png')}`} alt="" />
       </Navbar.Brand>
@@ -19,15 +27,15 @@ export const Header = () => {
       <Navbar.Collapse className='text-white'>
   
         <NavLink to={'/'}>
-          <Navbar.Link className='text-white hover:bg-purple-800'>Home</Navbar.Link>
+          <Navbar.Link className={`${navLinkDesign} text-white hover:bg-purple-800`}>Home</Navbar.Link>
         </NavLink>
 
         <NavLink to={'/about'}>
-          <Navbar.Link className='text-white hover:bg-purple-800'>About</Navbar.Link>
+          <Navbar.Link className={`${navLinkDesign} text-white hover:bg-purple-800`}>About</Navbar.Link>
         </NavLink>
 
         <NavLink to={'/education'}>
-          <Navbar.Link className='text-white hover:bg-purple-800'>Education</Navbar.Link>
+          <Navbar.Link className={`${navLinkDesign} text-white hover:bg-purple-800`}>Education</Navbar.Link>
         </NavLink>
 
       </Navbar.Collapse>
